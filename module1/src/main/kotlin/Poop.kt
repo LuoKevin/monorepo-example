06b25da15ad6c1f42ac3649c721cf9036851d8da
kevinluo <luokev1@gmail.com>
2021-04-20 11:41:57 -0400

    initial setup

---
+++ module1/src/main/kotlin/Poop.kt
@@ -0,0 +1,4 @@
+
+fun main() {
+    println("this should only build if changes are made 2 it")
+}
---
+++ module1/src/test/kotlin/Test.kt
@@ -0,0 +1,10 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Testes {
+
+    @Test
+    fun poopy() {
+        assertEquals(1,1)
+    }
+}
